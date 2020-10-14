4738d1bf3f6e6ab5320feaffec36413c097a35e7
Khurram Nizami <nizamik@amazon.com>
2020-10-14 08:51:04 -0500

    initial commit

---
+++ userdata_linux_basic.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# comment out the appropriate linux version for install
+
+# Amazon Linux
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
+# Redhat
+# curl -o /tmp/cwagent.rpm https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
+# SUSE
+# wget https://s3.amazonaws.com/amazoncloudwatch-agent/suse/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
+# Debian
+# wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb -O /tmp/cwagent.deb
+# Ubuntu
+# curl -o /tmp/cwagent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+
+# For RPM install, uncoment next line
+rpm -U /tmp/cwagent.rpm
+
+# For Debian package install, uncoment next line
+# dpkg -i -E ./cwagent.deb
+
+
+cat > /tmp/cwconfig.json <<"EOL"
+{
+	"agent": {
+		"metrics_collection_interval": 60,
+		"run_as_user": "root"
+	},
+	"metrics": {
+		"append_dimensions": {
+			"AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+			"ImageId": "${aws:ImageId}",
+			"InstanceId": "${aws:InstanceId}",
+			"InstanceType": "${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"disk": {
+				"measurement": [
+					"used_percent"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			}
+		}
+	}
+}
+EOL
+echo "Configuring CloudWatch agent with file /tmp/cwconfig.json: "
+cat /tmp/cwconfig.json
+echo "starting cloudwatch agent"
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/tmp/cwconfig.json -s
