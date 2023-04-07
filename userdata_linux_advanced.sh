6537f0ee47f534bb585bfa6e4521c4514ac6fef8
michael pechner <mike.pechner@akasa.com>
2023-04-07 15:16:30 -0700

    Instead of comments, have set each OS as a conditional include

--- userdata_linux_advanced.sh
+++ userdata_linux_advanced.sh
@@ -1,22 +1,52 @@
 #!/usr/bin/env bash
 # comment out the appropriate linux version for install
+source /etc/os-release
+if [[ $ID == 'amzn' ]] ; then
+  # Amazon Linux
+  wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
+  yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+  systemctl enable amazon-ssm-agent
+  systemctl restart amazon-ssm-agent
 
-# Amazon Linux
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
-# Redhat
-# curl -o /tmp/cwagent.rpm https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
-# SUSE
-# wget https://s3.amazonaws.com/amazoncloudwatch-agent/suse/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
-# Debian
-# wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb -O /tmp/cwagent.deb
-# Ubuntu
-# curl -o /tmp/cwagent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+elif [[ $ID == 'rhel' ]]; then
+  # Redhat
+  curl -o /tmp/cwagent.rpm https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm
+  yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+  systemctl enable amazon-ssm-agent
+  systemctl restart amazon-ssm-agent
+elif [[ $ID == 'sles' ]]; then
+  # SUSE
+  wget https://s3.amazonaws.com/amazoncloudwatch-agent/suse/amd64/latest/amazon-cloudwatch-agent.rpm -O /tmp/cwagent.rpm
+  wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+  rpm --install amazon-ssm-agent.rpm
+  systemctl enable amazon-ssm-agent
+  systemctl restart amazon-ssm-agent
 
-# For RPM install, uncoment next line
-rpm -U /tmp/cwagent.rpm
+elif [[ $ID == 'debian' ]]; then
+  # Debian
+  wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb -O /tmp/cwagent.deb
+  wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+  dpkg -i amazon-ssm-agent.deb
+  systemctl enable amazon-ssm-agent
+  systemctl restart amazon-ssm-agent
+elif [[ $ID == 'ubuntu' ]] ; then
+  # Ubuntu
+   curl -o /tmp/cwagent.deb https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+   snap switch --channel=candidate amazon-ssm-agent
+   sudo snap install amazon-ssm-agent --classic
+  systemctl enable amazon-ssm-agent
+  systemctl enable  snap.amazon-ssm-agent.amazon-ssm-agent.service
+  systemctl restart  snap.amazon-ssm-agent.amazon-ssm-agent.service
 
-# For Debian package install, uncoment next line
-# dpkg -i -E ./cwagent.deb
+fi
+
+if [[ $ID_LIKE == 'fedora' ]] || [[ $ID_LIKE == 'suse'  ]]; then
+  # For RPM install, uncoment next line
+  rpm -U /tmp/cwagent.rpm
+elif [[ $ID_LIKE == 'debian' ]] || [[ $ID == 'debian'  ]]; then
+  # For Debian package install, uncoment next line
+  dpkg -i -E ./cwagent.deb
+fi
 
 
 cat > /tmp/cwconfig.json <<"EOL"
