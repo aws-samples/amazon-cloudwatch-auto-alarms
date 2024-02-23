ee3e5e36649903637d7f78d987776e80edc36334
Khurram Nizami <knizami@gmail.com>
2024-02-22 18:03:22 -0600

    Implement wildcard support for alarm metric dimensions. (#54)
    Add daily event execution rule.

--- src/actions.py
+++ src/actions.py
@@ -213,80 +213,30 @@ def process_lambda_alarms(function_name, tags, activation_tag, default_alarms, s
                          dimensions, EvaluationPeriods, sns_topic_arn)
 
 
+# def create_alarm_from_wildcard_tag(instance_id, alarm_tag, instance_info, metric_dimensions_map, sns_topic_arn,
+#                                    alarm_separator, alarm_identifier):
+#     alarm_properties = alarm_tag['Key'].split(alarm_separator)
+#     namespace = alarm_properties[1]
+#     MetricName = alarm_properties[2]
+#     AlarmName = alarm_separator.join([alarm_identifier, id, namespace, MetricName])
+#
+#     dimensions, properties_offset, AlarmName = determine_dimensions(AlarmName, alarm_separator, alarm_tag, instance_info, metric_dimensions_map,
+#                                                                     namespace)
+
 def create_alarm_from_tag(id, alarm_tag, instance_info, metric_dimensions_map, sns_topic_arn, alarm_separator,
                           alarm_identifier):
     # split alarm tag to decipher alarm properties, first property is alarm_identifier and ignored...
     alarm_properties = alarm_tag['Key'].split(alarm_separator)
     namespace = alarm_properties[1]
     MetricName = alarm_properties[2]
-    dimensions = list()
-
-    # the number of dimensions may be different depending on the namespace.  For the default 'CWAgent' namespace, the default is to also include extended properties defined in cw_auto_alarms.py:append_dimensions
-    for dimension_name in metric_dimensions_map.get(namespace, list()):
-        dimension = dict()
-        # Evaluate the dimensions specified for the metric namespace
-        # If AutoScalingGroupName has been specified as a dimension to include, we first check to see if the instance has a tag indicating it is a part of an ASG.
-        # If it is, we get the ASG name and populate its value for this dimension.
-        if dimension_name == 'AutoScalingGroupName':
-            # find out if the instance is part of an autoscaling group
-            instance_asg = next(
-                (tag['Value'] for tag in instance_info['Tags'] if tag['Key'] == 'aws:autoscaling:groupName'), None)
-            if instance_asg:
-                dimension_value = instance_asg
-                dimension['Name'] = dimension_name
-                dimension['Value'] = dimension_value
-                dimensions.append(dimension)
-        else:
-            # If the dimension exists as a property of the EC2 instance being processed, get the dimension value from
-            # the EC2 instance details and add, otherwise issue a warning and skip.
-            dimension_value = instance_info.get(dimension_name, None)
-            if dimension_value:
-                dimension['Name'] = dimension_name
-                dimension['Value'] = dimension_value
-                dimensions.append(dimension)
-            else:
-                logger.warning(
-                    "Dimension {} has been specified in APPEND_DIMENSIONS but  no dimension value exists, skipping...".format(
-                        dimension_name))
-
-    logger.debug("dimensions are {}".format(dimensions))
-
-    additional_dimensions = list()
-
-    # determine the dimensions and the last dimension for this alarm
-    # exclude last element which is the DESCRIPTION that differentiates alarms
-    for index, prop in enumerate(alarm_properties[3:-1], start=3):
-        if prop in valid_comparators:
-            prop_end_index = index
-            break
-    else:
-        prop_end_index = None
-
-    if prop_end_index:
-        additional_dimensions.extend(alarm_properties[3:prop_end_index])
-    else:
-        logger.error('Unable to determine the dimensions for alarm tag: {}'.format(alarm_tag))
-        raise Exception
 
     AlarmName = alarm_separator.join([alarm_identifier, id, namespace, MetricName])
-    properties_offset = 0
-    # process the dimensions
-    try:
-        if additional_dimensions:
-            for num, dim in enumerate(additional_dimensions[::2]):
-                val = additional_dimensions[num * 2 + 1]
-                dimensions.append(
-                    {
-                        'Name': dim,
-                        'Value': val
-                    }
-                )
-                AlarmName += alarm_separator.join(['', dim, val])
-                properties_offset = properties_offset + 2
-    except Exception as e:
-        logger.error('Getting dimensions: {}'.format(e))
-        raise
 
+    dimensions, properties_offset, AlarmName = determine_dimensions(AlarmName, alarm_separator, alarm_tag,
+                                                                    instance_info, metric_dimensions_map,
+                                                                    namespace)
+
+    logger.info("dimensions: {}, properties_offset: {}, AlarmName: {}".format(dimensions, properties_offset, AlarmName))
     ComparisonOperator = alarm_properties[(properties_offset + 3)]
     Period = alarm_properties[(properties_offset + 4)]
 
@@ -317,7 +267,79 @@ def create_alarm_from_tag(id, alarm_tag, instance_info, metric_dimensions_map, s
                  dimensions, EvaluationPeriods, sns_topic_arn)
 
 
-def process_alarm_tags(instance_id, instance_info, default_alarms, metric_dimensions_map, sns_topic_arn, cw_namespace,
+def determine_dimensions(AlarmName, alarm_separator, alarm_tag, instance_info, metric_dimensions_map, namespace):
+    dimensions = list()
+    # the number of dimensions may be different depending on the namespace.  For the default 'CWAgent' namespace, the default is to also include extended properties defined in cw_auto_alarms.py:append_dimensions
+    for dimension_name in metric_dimensions_map.get(namespace, list()):
+        dimension = dict()
+        # Evaluate the dimensions specified for the metric namespace
+        # If AutoScalingGroupName has been specified as a dimension to include, we first check to see if the instance has a tag indicating it is a part of an ASG.
+        # If it is, we get the ASG name and populate its value for this dimension.
+        if dimension_name == 'AutoScalingGroupName':
+            # find out if the instance is part of an autoscaling group
+            instance_asg = next(
+                (tag['Value'] for tag in instance_info['Tags'] if tag['Key'] == 'aws:autoscaling:groupName'), None)
+            if instance_asg:
+                dimension_value = instance_asg
+                dimension['Name'] = dimension_name
+                dimension['Value'] = dimension_value
+                dimensions.append(dimension)
+        else:
+            # If the dimension exists as a property of the EC2 instance being processed, get the dimension value from
+            # the EC2 instance details and add, otherwise issue a warning and skip.
+            dimension_value = instance_info.get(dimension_name, None)
+            if dimension_value:
+                dimension['Name'] = dimension_name
+                dimension['Value'] = dimension_value
+                dimensions.append(dimension)
+            else:
+                logger.warning(
+                    "Dimension {} has been specified in APPEND_DIMENSIONS but  no dimension value exists, skipping...".format(
+                        dimension_name))
+    logger.debug("dimensions are {}".format(dimensions))
+    additional_dimensions = determine_additional_dimensions(alarm_tag, alarm_separator)
+    # process the dimensions
+    properties_offset = 0
+    try:
+        if additional_dimensions:
+            for num, dim in enumerate(additional_dimensions[::2]):
+                val = additional_dimensions[num * 2 + 1]
+                dimensions.append(
+                    {
+                        'Name': dim,
+                        'Value': val
+                    }
+                )
+                AlarmName += alarm_separator.join(['', dim, val])
+                properties_offset = properties_offset + 2
+    except Exception as e:
+        logger.error('Getting dimensions: {}'.format(e))
+        raise
+
+    return dimensions, properties_offset, AlarmName
+
+
+def determine_additional_dimensions(alarm_tag, alarm_separator):
+    alarm_properties = alarm_tag['Key'].split(alarm_separator)
+    additional_dimensions = list()
+    # determine the dimensions and the last dimension for this alarm
+    # exclude last element which is the DESCRIPTION that differentiates alarms
+    for index, prop in enumerate(alarm_properties[3:-1], start=3):
+        if prop in valid_comparators:
+            prop_end_index = index
+            break
+    else:
+        prop_end_index = None
+    if prop_end_index:
+        additional_dimensions.extend(alarm_properties[3:prop_end_index])
+    else:
+        logger.error('Unable to determine the dimensions for alarm tag: {}'.format(alarm_tag))
+        raise Exception
+    return additional_dimensions
+
+
+def process_alarm_tags(instance_id, instance_info, default_alarms, wildcard_alarms, metric_dimensions_map,
+                       sns_topic_arn, cw_namespace,
                        create_default_alarms_flag, alarm_separator, alarm_identifier):
     tags = instance_info['Tags']
 
@@ -351,10 +373,92 @@ def process_alarm_tags(instance_id, instance_info, default_alarms, metric_dimens
             for alarm_tag in default_alarms[cw_namespace][platform]:
                 create_alarm_from_tag(instance_id, alarm_tag, instance_info, metric_dimensions_map, sns_topic_arn,
                                       alarm_separator, alarm_identifier)
+            if wildcard_alarms and cw_namespace in wildcard_alarms and platform in wildcard_alarms[cw_namespace]:
+                for wildcard_alarm_tag in wildcard_alarms[cw_namespace][platform]:
+                    logger.info("processing wildcard tag {}".format(wildcard_alarm_tag))
+                    resolved_alarm_tags = determine_wildcard_alarms(wildcard_alarm_tag, alarm_separator,
+                                                                    instance_info, metric_dimensions_map)
+                    if resolved_alarm_tags:
+                        for resolved_alarm_tag in resolved_alarm_tags:
+                            create_alarm_from_tag(instance_id, resolved_alarm_tag, instance_info, metric_dimensions_map, sns_topic_arn,
+                                                  alarm_separator, alarm_identifier)
+                    else:
+                        logger.info("No wildcard alarms found for platform: {}".format(platform))
         else:
             logger.warning("Skipping platform specific alarm creation for {}, unknown platform.".format(instance_id))
     else:
         logger.info("Default alarm creation is turned off")
+
+
+def determine_wildcard_alarms(wildcard_alarm_tag, alarm_separator, instance_info, metric_dimensions_map):
+    fixed_alarm_tags = []
+    alarm_properties = wildcard_alarm_tag['Key'].split(alarm_separator)
+    namespace = alarm_properties[1]
+    MetricName = alarm_properties[2]
+
+    dimensions, properties_offset, AlarmName = determine_dimensions("", alarm_separator, wildcard_alarm_tag,
+                                                                    instance_info, metric_dimensions_map,
+                                                                    namespace)
+    logger.info("wildcard alarm: {}, dimensions: {}, properties offset: {}".format(wildcard_alarm_tag, dimensions,
+                                                                                   properties_offset))
+
+    wildcard_dimensions = [dimension for dimension in dimensions if dimension['Value'] == '*']
+    fixed_dimensions = [dimension for dimension in dimensions if dimension['Value'] != '*']
+
+    logger.info("original dimensions: ${}, wildcard_dimensions: {},\nfixed_dimensions: {}".format(dimensions,wildcard_dimensions, fixed_dimensions))
+
+    cw_client = boto3_client('cloudwatch')
+    listmetrics_response = cw_client.list_metrics(
+        Namespace=namespace,
+        MetricName=MetricName,
+        Dimensions=fixed_dimensions,
+        # IncludeLinkedAccounts=False,
+        # OwningAccount='string'
+    )
+
+    logger.info("list_metrics response: {}".format(listmetrics_response))
+
+    metrics_for_wildcards = None
+    if 'Metrics' in listmetrics_response:
+        metrics_for_wildcards = listmetrics_response['Metrics']
+        for metric in metrics_for_wildcards:
+            translated_alarm = dict()
+            original_alarm_key = alarm_properties.copy()
+            metric_dimensions = metric['Dimensions']
+            for wildcard_dim in wildcard_dimensions:
+                wildcard_dim['Name']
+                for metric_dim in metric_dimensions:
+                    if metric_dim['Name'] == wildcard_dim['Name']:
+                        original_dim_value_index = original_alarm_key.index(metric_dim['Name']) + 1
+                        original_alarm_key[original_dim_value_index] = metric_dim['Value']
+
+            translated_alarm['Key'] = alarm_separator.join(original_alarm_key)
+            translated_alarm['Value'] = wildcard_alarm_tag['Value']
+            fixed_alarm_tags.append(translated_alarm)
+
+    logger.info("fixed alarm tags are: {}".format(fixed_alarm_tags))
+    return fixed_alarm_tags
+
+
+
+
+
+
+
+
+
+
+
+        # {
+        #     'Key': alarm_separator.join(
+        #         [alarm_identifier, cw_namespace, 'LogicalDisk % Free Space', 'objectname', 'LogicalDisk',
+        #          'instance', 'C:', 'LessThanThreshold', default_period, default_evaluation_periods,
+        #          default_statistic, 'Created_by_CloudWatchAutoAlarms']),
+        #     'Value': alarm_disk_space_percent_free_threshold
+        # },
+
+
+
 
 
 def determine_platform(imageid):
@@ -529,13 +633,16 @@ def scan_and_process_alarm_tags(create_alarm_tag, default_alarms, metric_dimensi
                                 cw_namespace, create_default_alarms_flag, alarm_separator, alarm_identifier):
     try:
         ec2_client = boto3_client('ec2')
+        default_filtered_alarms, wildcard_alarms = separate_wildcard_alarms(alarm_separator, cw_namespace,
+                                                                            default_alarms)
         for reservation in ec2_client.describe_instances()["Reservations"]:
             for instance in reservation["Instances"]:
                 # Look for running instances only
                 if instance["State"]["Code"] > 16:
                     continue
                 if check_alarm_tag(instance["InstanceId"], create_alarm_tag):
-                    process_alarm_tags(instance["InstanceId"], instance, default_alarms, metric_dimensions_map,
+                    process_alarm_tags(instance["InstanceId"], instance, default_filtered_alarms, wildcard_alarms,
+                                       metric_dimensions_map,
                                        sns_topic_arn, cw_namespace, create_default_alarms_flag, alarm_separator,
                                        alarm_identifier)
 
@@ -544,3 +651,24 @@ def scan_and_process_alarm_tags(create_alarm_tag, default_alarms, metric_dimensi
         # then fail and log the exception message.
         logger.error('Failure describing reservations : {}'.format(e))
         raise
+
+
+def separate_wildcard_alarms(alarm_separator, cw_namespace, default_alarms):
+    wildcard_alarms = dict()
+    wildcard_alarms[cw_namespace] = dict()
+    for platform in default_alarms[cw_namespace]:
+        wildcard_alarms[cw_namespace][platform] = list()
+        logger.info("default alarms for {} are {}".format(platform, default_alarms[cw_namespace][platform]))
+        wildcard_alarms[cw_namespace][platform] = [alarm for alarm in default_alarms[cw_namespace][platform] if
+                                                   '*' in alarm['Key'].split(alarm_separator)]
+        default_alarms[cw_namespace][platform] = [alarm for alarm in default_alarms[cw_namespace][platform] if
+                                                  '*' not in alarm['Key'].split(alarm_separator)]
+    logger.info("updated default alarms are {}".format(default_alarms[cw_namespace]))
+    logger.info("updated wildcard alarms are {}".format(wildcard_alarms[cw_namespace]))
+    return default_alarms, wildcard_alarms
+
+
+def process_wildcard_alarm(alarm_object):
+    print("alarm object is {}".format(alarm_object))
+    alarm_object['Key'] = "updated"
+    return alarm_object
